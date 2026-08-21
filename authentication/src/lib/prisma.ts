41d00d0d2726cc491a85a2ba29e94f4e95c3c752
Nuon-Hokseng <nuon.hokseng24@kit.edu.kh>
2026-08-21 22:07:03 +0700

    fix database url error

--- authentication/src/lib/prisma.ts
+++ authentication/src/lib/prisma.ts
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 import { PrismaClient } from "@genie/prisma-client";
 import { PrismaPg } from "@prisma/adapter-pg";
@@ -18,3 +20,4 @@ export const prismaClientOptions = { adapter };
 export function createPrismaClient() {
   return new PrismaClient(prismaClientOptions);
 }
+
